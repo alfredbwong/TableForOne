05e008432d2d669dcb8e3a1326bb2a3e96cfc47d
Alfred <alfred.b.wong@gmail.com>
2021-04-26 16:54:07 -0400

    Remove some logs add some padding

--- app/src/main/java/android/example/tableforone/utils/NetworkUtils.kt
+++ app/src/main/java/android/example/tableforone/utils/NetworkUtils.kt
@@ -8,15 +8,12 @@ fun parseAsteroidsJsonResult(jsonResult: JSONObject): ArrayList<MealCategory> {
     val mealCategoriesJson = jsonResult.getJSONArray("categories")
     val mealCategoryList = ArrayList<MealCategory>()
 
-    Log.i("NetworkUtils", mealCategoriesJson.toString())
-
     for ( i in 0 until mealCategoriesJson.length()){
         val mealCategoryObj = mealCategoriesJson.getJSONObject(i)
         val id = mealCategoryObj.getString("idCategory")
         val category = mealCategoryObj.getString("strCategory")
         val imgUrl = mealCategoryObj.getString("strCategoryThumb")
         val mealCategory = MealCategory(id, category, imgUrl)
-        Log.i("Util", "$mealCategory")
         mealCategoryList.add(mealCategory)
     }
     return mealCategoryList
