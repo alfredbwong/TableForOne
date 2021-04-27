ff84b93cf6cafa05f7d150fafdd43162a8ce01da
Alfred <alfred.b.wong@gmail.com>
2021-04-27 16:10:19 -0400

    Refactor time date select so logic is in viewmodel

--- app/src/main/java/android/example/tableforone/TimeDateSelectFragment.kt
+++ app/src/main/java/android/example/tableforone/TimeDateSelectFragment.kt
@@ -3,6 +3,7 @@ package android.example.tableforone
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
 import android.example.tableforone.databinding.FragmentTimeDateSelectBinding
+import android.example.tableforone.mealCateorySelect.MealCategorySelectViewModel
 import android.os.Bundle
 import android.text.format.DateFormat
 import androidx.fragment.app.Fragment
@@ -11,7 +12,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.DatePicker
 import android.widget.TimePicker
+import androidx.fragment.app.activityViewModels
 import java.util.*
+import androidx.lifecycle.Observer
+
 
 
 /**
@@ -21,51 +25,51 @@ import java.util.*
  */
 class TimeDateSelectFragment : Fragment(), DatePickerDialog.OnDateSetListener,
         TimePickerDialog.OnTimeSetListener {
-    var day = 0
-    var month: Int = 0
-    var year: Int = 0
-    var hour: Int = 0
-    var minute: Int = 0
-    var myDay = 0
-    var myMonth: Int = 0
-    var myYear: Int = 0
-    var myHour: Int = 0
-    var myMinute: Int = 0
+    private val viewModel: MealCategorySelectViewModel by activityViewModels()
+
     private lateinit var binding: FragmentTimeDateSelectBinding
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         binding = FragmentTimeDateSelectBinding.inflate(inflater)
         binding.dateTimeSelectButton.setOnClickListener{
-            val calendar: Calendar = Calendar.getInstance()
-            day = calendar.get(Calendar.DAY_OF_MONTH)
-            month = calendar.get(Calendar.MONTH)
-            year = calendar.get(Calendar.YEAR)
-            val datePickerDialog =
-                    DatePickerDialog(requireContext(), this, year, month,day)
-            datePickerDialog.show()
+            viewModel.setupDatePicker()
         }
+
+        viewModel.showDatePicker.observe(viewLifecycleOwner, Observer {
+            showDatePicker->
+            if (showDatePicker){
+                val datePickerDialog =
+                        DatePickerDialog(requireContext(), this, viewModel.year, viewModel.month,viewModel.day)
+                datePickerDialog.show()
+            }
+
+        })
+
+        viewModel.showTimePicker.observe(viewLifecycleOwner, Observer {
+            showTimePicker->
+            if (showTimePicker){
+                val timePickerDialog = TimePickerDialog(requireContext(), this, viewModel.hour, viewModel.minute,
+                        DateFormat.is24HourFormat(requireContext()))
+                timePickerDialog.show()
+            }
+
+        })
         return binding.root
     }
 
-    companion object {
-        const val TAG = "TimeDateFrag"
-    }
 
     override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
-        myDay = day
-        myYear = year
-        myMonth = month
-        val calendar: Calendar = Calendar.getInstance()
-        hour = calendar.get(Calendar.HOUR)
-        minute = calendar.get(Calendar.MINUTE)
-        val timePickerDialog = TimePickerDialog(requireContext(), this, hour, minute,
-                DateFormat.is24HourFormat(requireContext()))
-        timePickerDialog.show()
+        viewModel.onDateSetFun( year, month, dayOfMonth)
     }
 
     override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
-        myHour = hourOfDay
-        myMinute = minute
-        binding.textView.text = "Year: " + myYear + "\n" + "Month: " + myMonth + "\n" + "Day: " + myDay + "\n" + "Hour: " + myHour + "\n" + "Minute: " + myMinute
+        viewModel.onTimeSetFun(hourOfDay, minute)
+
+        binding.textView.text = "Year: " + viewModel.myYear + "\n" + "Month: " + viewModel.myMonth + "\n" + "Day: " + viewModel.myDay + "\n" + "Hour: " + viewModel.myHour + "\n" + "Minute: " + viewModel.myMinute
+    }
+
+
+    companion object {
+        const val TAG = "TimeDateFrag"
     }
 }
--- app/src/main/java/android/example/tableforone/mealCateorySelect/MealCategorySelectViewModel.kt
+++ app/src/main/java/android/example/tableforone/mealCateorySelect/MealCategorySelectViewModel.kt
@@ -11,6 +11,7 @@ import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.util.*
 
 class MealCategorySelectViewModel : ViewModel() {
 
@@ -29,8 +30,30 @@ class MealCategorySelectViewModel : ViewModel() {
     val mealItems: LiveData<List<MealSelectItem>>
         get() = _mealItems
 
+    private var _showDatePicker = MutableLiveData<Boolean>()
+
+    val showDatePicker: LiveData<Boolean>
+        get() = _showDatePicker
+
+    private var _showTimePicker = MutableLiveData<Boolean>()
+
+    val showTimePicker: LiveData<Boolean>
+        get() = _showTimePicker
+
+    var day = 0
+    var month: Int = 0
+    var year: Int = 0
+    var hour: Int = 0
+    var minute: Int = 0
+    var myDay = 0
+    var myMonth: Int = 0
+    var myYear: Int = 0
+    var myHour: Int = 0
+    var myMinute: Int = 0
+
     init {
         getMealCategoriesResponse()
+        _showDatePicker.value = false
     }
 
     private fun getMealCategoriesResponse() {
@@ -61,6 +84,33 @@ class MealCategorySelectViewModel : ViewModel() {
                 }
             })
         }
+    }
+
+    fun setupDatePicker() {
+        val calendar: Calendar = Calendar.getInstance()
+        day = calendar.get(Calendar.DAY_OF_MONTH)
+        month = calendar.get(Calendar.MONTH)
+        year = calendar.get(Calendar.YEAR)
+        _showDatePicker.value = true
+        _showTimePicker.value = false
+    }
+
+    fun onDateSetFun(year: Int, month: Int, dayOfMonth: Int) {
+        myDay = dayOfMonth
+        myYear = year
+        myMonth = month
+        val calendar: Calendar = Calendar.getInstance()
+        hour = calendar.get(Calendar.HOUR)
+        minute = calendar.get(Calendar.MINUTE)
+        _showDatePicker.value = false
+        _showTimePicker.value = true
+    }
+
+    fun onTimeSetFun(hourOfDay: Int, minute: Int) {
+        myHour = hourOfDay
+        myMinute = minute
+        _showDatePicker.value = false
+        _showTimePicker.value = false
     }
 
 }
