b8ee5be94f76cd3994b7ea9b133a894f80e94538
Alfred <alfred.b.wong@gmail.com>
2021-05-04 13:23:27 -0400

    Add permission check

--- app/src/main/java/android/example/tableforone/MainActivity.kt
+++ app/src/main/java/android/example/tableforone/MainActivity.kt
@@ -1,41 +1,61 @@
 package android.example.tableforone
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
-import android.example.tableforone.meal.database.MealCategoryDatabase
-import android.example.tableforone.network.MealApiService
+import android.example.tableforone.databinding.ActivityMainBinding
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.net.NetworkInfo
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import androidx.databinding.DataBindingUtil
+import com.google.android.material.snackbar.Snackbar
+
+
 const val BASE_URL = "https://www.themealdb.com/"
 
 class MainActivity : AppCompatActivity() {
 
-
-
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
-        setupPermissions()
+        isInternetConnected()
+
     }
 
-    private fun setupPermissions() {
-        val permission = ContextCompat.checkSelfPermission(this,
-                Manifest.permission.INTERNET)
+    private fun turnOnInternetPermission() {
+        Snackbar.make(
+                binding.root,
+                R.string.internet_required_error, Snackbar.LENGTH_INDEFINITE
+        ).setAction(android.R.string.ok) {
+            isInternetConnected()
+        }.show()
+    }
 
-        if (permission != PackageManager.PERMISSION_GRANTED) {
-            Log.i(TAG, "Permission to record denied")
+    fun isInternetConnected()  {
+        var isInternetConnected = false
+        val cm = applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager?
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            cm?.run {
+                cm.getNetworkCapabilities(cm.activeNetwork)?.run {
+                    isInternetConnected = hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+                }
+            }
+        }
+        if (!isInternetConnected){
+            turnOnInternetPermission()
         }
     }
-
     companion object{
         const val TAG = "MainActivity"
     }
+
 
 }
