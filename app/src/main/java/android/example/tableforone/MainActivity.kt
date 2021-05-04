e160aa4c07518f5c5c1b536f4ee68f4c0325e587
Alfred <alfred.b.wong@gmail.com>
2021-05-04 12:04:38 -0400

    Add adjust view bounds

--- app/src/main/java/android/example/tableforone/MainActivity.kt
+++ app/src/main/java/android/example/tableforone/MainActivity.kt
@@ -1,9 +1,15 @@
 package android.example.tableforone
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.example.tableforone.meal.database.MealCategoryDatabase
 import android.example.tableforone.network.MealApiService
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
 const val BASE_URL = "https://www.themealdb.com/"
@@ -15,5 +21,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupPermissions()
     }
+
+    private fun setupPermissions() {
+        val permission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.INTERNET)
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            Log.i(TAG, "Permission to record denied")
+        }
+    }
+
+    companion object{
+        const val TAG = "MainActivity"
+    }
+
 }
